3e83ecd6db9204c3ccdda570ad3070da75055f6c
Crash-afftar <sashakhymenko88@gmail.com>
2023-02-28 23:58:23 +0200

    HW-9.1-28022023

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+    const body = document.body;
+    const startBtn = document.querySelector('[data-start]');
+    const stopBtn = document.querySelector('[data-stop]');
+
+    const PROMPT_DELAY = 1000;
+    let intervalId = null;
+
+    startBtn.addEventListener('click', onStartButtonClick);
+    stopBtn.addEventListener('click', onStopButtonClick);
+
+    function onStartButtonClick() {
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+
+        intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        },
+        PROMPT_DELAY);
+    }
+    
+        
+    function onStopButtonClick() {
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+        
+        clearInterval(intervalId);
+        body.style.backgroundColor = 'transparent';
+    }
